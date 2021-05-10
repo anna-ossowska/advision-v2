f48391c2831e722527fd36ce0e6034ce59e1a1ef
Anna Ossowska <ossowska.anna1@gmail.com>
2021-05-10 19:20:13 +0200

    Adding js

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Smooth Scrolling
+$('#navigation a, .btn--main').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      1300
+    );
+  }
+});
+
+// Navigation switch
+const navigation = document.getElementById('navigation');
+let scrolled = false;
+
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    navigation.classList.remove('navigation--top');
+    if (!scrolled) {
+      navigation.style.transform = 'translateY(-70px)';
+    }
+    setTimeout(function () {
+      navigation.style.transform = 'translateY(0)';
+      scrolled = true;
+    }, 200);
+  } else {
+    navigation.classList.add('navigation--top');
+    scrolled = false;
+  }
+};
